f27e1fc7eda04f223c63d6ca20c584072fc510dd
HoWoong <dlghdnd@gmail.com>
2023-12-27 05:28:35 +0900

    초기 커밋.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfApp1.ViewModels;
+using WpfApp1.Views;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			Services = ConfigureServices();
+			Startup += App_Startup;
+		}
+
+		private void App_Startup(object sender, StartupEventArgs e)
+		{
+			var mainView = App.Current.Services.GetService<MainView>();
+			mainView.Show();
+		}
+
+		/// <summary>
+		/// Gets the current <see cref="App"/> instance in use
+		/// </summary>
+		public new static App Current => (App)Application.Current;
+
+		/// <summary>
+		/// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+		/// </summary>
+		public IServiceProvider Services { get; }
+
+		/// <summary>
+		/// Configures the services for the application.
+		/// </summary>
+		private static IServiceProvider ConfigureServices()
+		{
+			var services = new ServiceCollection();
+
+			//ViewModels
+			services.AddTransient<MainViewModel>();
+			services.AddTransient<LoginControlViewModel>();
+			services.AddTransient<SignupControlViewModel>();
+			services.AddTransient<ChangePwdControlViewModel>();
+
+			// Services
+			//services.AddSingleton<ITestService, TestService>();
+
+			// Views
+			services.AddSingleton<MainView>();
+
+			return services.BuildServiceProvider();
+		}
+	}
+
+}
---
+++ WpfLib/Extensions/FindParentExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfLib.Extensions
+{
+    public static class FindParentExtension
+    {
+		public static T? FindParent<T>(this DependencyObject child)
+		where T : DependencyObject
+		{
+			return FindParent<T>(child, null);
+		}
+
+		public static T? FindParent<T>(this DependencyObject child, string? parentName)
+        where T : DependencyObject
+		{
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent == null) 
+            {
+                return null;
+            }
+
+            var frameworkElement = (FrameworkElement) parent;
+
+            if ((parentName == null || frameworkElement.Name == parentName) && frameworkElement is T)
+            {
+                return (T)parent;
+            }
+            else 
+            {
+                return FindParent<T>(parent, parentName);
+            }
+        }
+    }
+}
