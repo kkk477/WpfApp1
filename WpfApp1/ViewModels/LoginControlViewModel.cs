59825e7bd240249fe2437703c93b87693cd8b0fd
HoWoong <dlghdnd@gmail.com>
2023-12-28 05:34:39 +0900

    사용자 정의 컨트롤(UserControl)을 만들어봐요!

--- WpfApp1/ViewModels/LoginControlViewModel.cs
+++ WpfApp1/ViewModels/LoginControlViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,16 @@ namespace WpfApp1.ViewModels
     [ObservableObject]
     public partial class LoginControlViewModel
     {
+        [ObservableProperty]
+        private ObservableCollection<String> _emails;
+
+        public LoginControlViewModel()
+        {
+            Emails = new ObservableCollection<String>()
+            {
+                "test1",
+                "test2"
+            };
+        }
     }
 }
